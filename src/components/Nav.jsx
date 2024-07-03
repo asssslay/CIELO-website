1466b01d32c43dc0ed791af2db7cf8f0ec5138de
asssslay <avramova.ruslana.a.r.r@gmail.com>
2024-07-03 12:13:05 +0300

    Fixed images import

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,30 @@
+import { assetPaths, navLinks } from "../constants";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <header className="pl-28 py-5 border border-cyan-300">
+      <nav className="flex justify-between items-center max-container ml-0 border border-purple-300 bg-slate-600">
+        <a href="/">
+          <img src={assetPaths.headerLogo} alt="Logo" width={98} height={32} />
+        </a>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a
+                className="font-manrope leading-normal text-lg text-white"
+                href={link.href}
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        {/* hamburger menu */}
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const assetPaths = {
+  headerLogo: "/assets/icons/headerLogo.svg",
+  footerLogo: "/assets/icons/footerLogo.svg",
+  exploreIcon: "/assets/icons/exploreIcon.svg",
+  quotes: "/assets/icons/quotes.svg",
+};
+export const navLinks = [
+  { href: "#", label: "Products" },
+  { href: "#-us", label: "Services" },
+  { href: "#", label: "About" },
+  { href: "#-us", label: "Contact" },
+];
