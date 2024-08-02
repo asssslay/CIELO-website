20b1280c26ae777afa4a1f443d5785c0e36a1026
asssslay <avramova.ruslana.a.r.r@gmail.com>
2024-08-02 15:21:41 +0300

    Bug fix -margin 2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,15 @@
 const Button = ({ label, iconURL }) => {
   return (
-    <button className="flex justify-center items-center gap-2 font-manrope text-[26px] uppercase leading-none text-white">
+    <button className="flex justify-center items-center gap-2 font-manrope text-xl md:text-2xl lg:text-[26px] uppercase leading-none text-white">
       {label}
 
-      {iconURL && <img src={iconURL} alt="explore icon" className=" w-8 h-8" />}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="explore icon"
+          className="w-7 h-7 md:w-8 md:h-8"
+        />
+      )}
     </button>
   );
 };
