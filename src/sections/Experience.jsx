62643be1b12e7e0da255cf44ecda2cc77c85ba8e
asssslay <avramova.ruslana.a.r.r@gmail.com>
2024-08-02 15:12:18 +0300

    Bag fix -margin

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -4,7 +4,7 @@ import { assetPaths } from "../constants";
 const Experience = () => {
   return (
     <section className="relative flex container">
-      <div className="mt-[156px] pl-4 xs:pl-8 md:pl-16 lg:pl-[132px] mr-8 z-10">
+      <div className="mt-16 xs:mt-24 md:mt-32 lg:mt-[156px] padding-left mr-8 z-10">
         <h2 className="h2-custom mb-10">
           We provide you the
           <br />
@@ -13,7 +13,7 @@ const Experience = () => {
         <Statistics />
       </div>
       <img
-        className="hidden md:flex md:absolute right-12 top-[164px] max-w-[300px] sm:max-w-[400px] md:max-w-[500px] xl:max-w-[608px] max-h-[650px]"
+        className="hidden md:flex md:absolute right-12 top-10 md:top-32 lg:top-[164px] max-w-[300px] sm:max-w-[400px] md:max-w-[500px] xl:max-w-[608px] max-h-[650px]"
         src={assetPaths.experience}
         alt="Experience background"
       />
--- src/sections/FoundersNote.jsx
+++ src/sections/FoundersNote.jsx
@@ -2,25 +2,27 @@ import { assetPaths } from "../constants";
 
 const FoundersNote = () => {
   return (
-    <section className="container pl-[52px] mt-[272px] mb-[180px] flex flex-row gap-2 relative">
-      <div className="flex flex-col gap-8">
-        <img src={assetPaths.quotes} className="max-w-12 max-h-12" />
-        <p className="max-w-66ch desc-custom font-manrope leading-[40px]">
-          Every piece of furniture that we make is signed by the person who made
-          it, and the amount of pride that goes into that signature is
-          unbelievable. You know, there are so few people in America today whose
-          work is going to be understood and appreciated by their great-great
-          grandchildren who have not yet been born. And so we have that
-          advantage.
-        </p>
-      </div>
-      <div className="flex gap-8 items-center justify-center absolute bottom-[-70px] right-8">
-        <img
-          src={assetPaths.founder}
-          className="max-w-10 max-h-10 xs:max-w-12 xs:max-h-12 md:max-w-14 md:max-h-14 lg:max-w-16 lg:max-h-16 xl:max-w-20 xl:max-h-20"
-          alt="Founder photo"
-        />
-        <p className="desc-custom font-ubuntu">Leslie Alexander</p>
+    <section className="container mt-28 xs:mt-36 md:mt-[272px] mb-[180px] flex flex-row gap-2 relative">
+      <div className="padding-left">
+        <div className="flex flex-col gap-8">
+          <img src={assetPaths.quotes} className="max-w-12 max-h-12" />
+          <p className="max-w-66ch desc-custom font-manrope leading-[40px]">
+            Every piece of furniture that we make is signed by the person who
+            made it, and the amount of pride that goes into that signature is
+            unbelievable. You know, there are so few people in America today
+            whose work is going to be understood and appreciated by their
+            great-great grandchildren who have not yet been born. And so we have
+            that advantage.
+          </p>
+        </div>
+        <div className="flex gap-8 items-center justify-center absolute bottom-[-70px] right-8">
+          <img
+            src={assetPaths.founder}
+            className="max-w-10 max-h-10 xs:max-w-12 xs:max-h-12 md:max-w-14 md:max-h-14 lg:max-w-16 lg:max-h-16 xl:max-w-20 xl:max-h-20"
+            alt="Founder photo"
+          />
+          <p className="desc-custom font-ubuntu">Leslie Alexander</p>
+        </div>
       </div>
     </section>
   );
