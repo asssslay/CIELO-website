ab2dff6601b4da2e7a8ee1d2410405d478f71006
asssslay <avramova.ruslana.a.r.r@gmail.com>
2024-08-04 16:40:04 +0300

    Complete benefits section

--- src/sections/Benefits.jsx
+++ src/sections/Benefits.jsx
@@ -3,12 +3,9 @@ import BenefitsList from "../components/BenefitsList";
 
 const Benefits = () => {
   return (
-    <section className="container flex gap-[60px] mb-[100px] padding-left xl:pl-8 2xl:padding-left">
-      <img
-        src={assetPaths.benefits}
-        className="max-w-44 max-h-48 xs:max-h-[270px] sm:max-w-64 sm:max-h-[360px] md:max-w-[325px] md:max-h-[450px] lg:max-w-[425px] lg:max-h-[600px] xl:max-w-[525px] xl:max-h-[690px]"
-      />
-      <div className="flex flex-col gap-10">
+    <section className="container flex gap-4 xs:gap-8 md:gap-12 lg:gap-[60px] mb-[100px] padding-left xl:pl-8 2xl:padding-left">
+      <img src={assetPaths.benefits} className="benefits-img" />
+      <div className="flex flex-col gap-7 xs:gap-8 md:gap-9 lg:gap-10">
         <h2 className="font-cabinet text-2xl xs:text-3xl md:text-[42px] lg:text-7xl xl:text-[80px] leading-none ">
           Benefits you get
           <br className="max-xl:hidden" /> when our services.
--- src/sections/UsedMaterials.jsx
+++ src/sections/UsedMaterials.jsx
@@ -2,14 +2,14 @@ import { assetPaths } from "../constants";
 
 const UsedMaterials = () => {
   return (
-    <section className="container mb-[144px] padding-left">
-      <h3 className="font-cabinet text-xl xs:text-2xl md:text-4xl lg:text-6xl xl:text-7xl mb-14">
+    <section className="container mb-28 xs:mb-32 lg:mb-[144px] padding-left pr-6">
+      <h3 className="font-cabinet text-xl xs:text-2xl md:text-4xl lg:text-6xl xl:text-7xl mb-10 lg:mb-14">
         We use quality and
         <br />
         simple materials
       </h3>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-11">
-        <div className="flex flex-col gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 max-md:gap-y-8 lg:gap-11">
+        <div className="flex flex-col gap-4 xs:gap-5 sm:gap-6">
           <img src={assetPaths.woodMaterial} className="" />
           <p className="text-base xs:text-lg md:text-xl xl:text-2xl font-manrope">
             Natural wood
